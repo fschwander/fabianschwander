d78e6cdc4a2b2e565a687c51e609ee3551942ff0
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-04-10 11:09:18 +0200

    refactoring: created own component for ulr items

--- src/components/ItemsContainer.tsx
+++ src/components/ItemsContainer.tsx
@@ -1,18 +1,14 @@
 import './ItemsContainer.scss';
 import React from "react";
 import {urlItems} from '../res/urlItemsData';
-import {Col, Container, Row} from 'reactstrap';
+import {Container, Row} from 'reactstrap';
+import {UrlItem} from './UrlItem';
 
 export const ItemsContainer: React.FC = () => {
 
   const createUrlItems = () => {
     return urlItems.map((el, i) => {
-      return <Col className={'item'} key={el.name + i}
-                  xs={12} sm={6} md={4}
-      onClick={() => window.open(el.url, '_blank')}>
-        <h2>{el.name}</h2>
-        <p>{el.description}</p>
-      </Col>;
+      return <UrlItem data={el} index={i}/>
     });
   };
 
---
+++ src/components/UrlItem.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Col} from 'reactstrap';
+import './UrlItem.scss';
+
+interface UrlItemProps {
+  data: any,
+  index: number
+}
+
+export const UrlItem: React.FC<UrlItemProps> = props => {
+  const {data, index} = props;
+  return (
+    <Col className={'UrlItem'} key={data.name + index}
+         xs={12} sm={6} md={4}
+         onClick={() => window.open(data.url, '_blank')}>
+      <h2>{data.name}</h2>
+      <p>{data.description}</p>
+    </Col>
+  );
+};
