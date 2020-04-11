84f85537172cc32bfbb98f9d2d2299dc61c38859
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-04-11 15:28:34 +0200

    updated content/descriptions of work

--- src/res/urlItemsData.tsx
+++ src/res/urlItemsData.tsx
@@ -7,37 +7,37 @@ interface UrlItemsInterface {
 export const urlItems:Array<UrlItemsInterface> = [
   {
     name: "Work Life Aargau",
-    description: "Bachelorthesis",
+    description: "Exploratives Interaktionskonzept für den Kanton Aargau. Für Desktop. Bachelorarbeit",
     url: "http://work-life-aargau.fabianschwander.ch"
   },
   {
-    name: "Michelle Akanji",
-    description: "Portfolio von Michelle Akanji",
+    name: "Portfolio von Michelle Akanji",
+    description: "Audio-Feature, Artikel und Texte von Michelle Akanji",
     url: "http://michelleakanji.ch"
   },
   {
-    name: "Strukturenwandel in der Landwirtschaft",
-    description: "Informationsvisualisierung",
+    name: "Das grosse Hofsterben",
+    description: "Informationsvisualisierung über den Strukturenwandel in der Schweizer Landwirtschaft",
     url: "http://hofsterben.fabianschwander.ch"
   },
   {
-    name: "Modulplaner",
-    description: "Interaktive Modulvisualisierung",
+    name: "Modulplaner für Studierende",
+    description: "Interaktives Tool für die Studiumsplanung an der Fachhochschule Nordwestschweiz",
     url: "https://web0.fhnw.ch/modulvisualisierung/"
   },
   {
     name: "Isabel Akanji",
-    description: "Produkte aus den Bergen",
+    description: "Yoga und Produkte aus den Bergen",
     url: "http://isabelakanji.ch"
   },
   {
     name: "Tavolata für innere Werte",
-    description: "Dinieren von Kopf bis Fuss",
+    description: "Dinieren von Kopf bis Fuss an der Tavolata für innere Werte",
     url: "http://innere-werte.ch"
   },
   {
     name: "Ponsyrus Genossenschaft",
-    description: "Kleingenossenschaftliches Wohnen",
+    description: "Kleingenossenschaftliches Wohnen seit 1984. Landingpage für die Genossenschaft",
     url: "http://ponsyrus.ch"
   }
 ];
